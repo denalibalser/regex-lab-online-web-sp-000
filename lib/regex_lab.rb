6bd7e73fef94fbe86d3fbc79bf9c749620869eda
Denali Balser <denali.balser167@gmail.com>
2020-02-24 14:51:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  word=text.split(" ")
+  word.scan(/\b[A-Z].*?\b/)
 end
 
 def valid_phone_number?(phone)
